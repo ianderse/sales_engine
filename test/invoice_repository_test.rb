cb1666872fb34a34292fb644c86161e85fd5bfb3
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-08-21 13:38:05 -0600

    fixed invoice_repository_test

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -6,11 +6,13 @@ class InvoiceRepositoryTest < Minitest::Test
   attr_reader :repository
 
 	def setup
-    @repository = InvoiceRepository.new(SalesEngine.new)
+		@engine = SalesEngine.new
+		invoice_attributes = [{id: 1,customer_id: 1, merchant_id: 26, status: "shipped", created_at: "2012-03-25 09:54:09 UTC", updated_at: "2012-03-25 09:54:09 UTC"}]
+    @repository = InvoiceRepository.new(@engine, invoice_attributes)
 	end
 
 	def test_it_can_return_all
-	  assert_equal 4843, repository.all.count
+	  assert_equal 1, repository.all.count
 	end
 
 	def test_it_can_return_a_random_instance
@@ -18,42 +20,42 @@ class InvoiceRepositoryTest < Minitest::Test
 	end
 
 	def test_it_finds_by_id
-		results = repository.find_by_id("1")
-		assert_equal "26", results.merchant_id
+		results = repository.find_by_id(1)
+		assert_equal 26, results.merchant_id
 	end
 
 	def test_it_finds_by_customer_id
-		results = repository.find_by_customer_id("3")
-		assert_equal "10", results.id
+		results = repository.find_by_customer_id(1)
+		assert_equal 1, results.id
 	end
 
 	def test_it_finds_by_merchant_id
-		results = repository.find_by_merchant_id("43")
-		assert_equal "9", results.customer_id
+		results = repository.find_by_merchant_id(26)
+		assert_equal 1, results.customer_id
 	end
 
 	def test_it_finds_by_status
 		results = repository.find_by_status("shipped")
-		assert_equal "26", results.merchant_id
+		assert_equal 26, results.merchant_id
 	end
 
 	def test_it_finds_by_created_at
-		results = repository.find_by_created_at("2012-03-06 21:54:10 UTC")
-		assert_equal "86", results.merchant_id
+		results = repository.find_by_created_at("2012-03-25 09:54:09 UTC")
+		assert_equal 26, results.merchant_id
 	end
 
 	def test_it_finds_all_by_customer_id
-	  results = repository.find_all_by_customer_id("10")
-	  assert_equal 8, results.count
+	  results = repository.find_all_by_customer_id(1)
+	  assert_equal 1, results.count
 	end
 
 	def test_it_finds_all_by_merchant_id
-		results = repository.find_all_by_merchant_id("33")
-		assert_equal 55, results.count
+		results = repository.find_all_by_merchant_id(26)
+		assert_equal 1, results.count
 	end
 
 	def test_it_finds_all_by_status
 		results = repository.find_all_by_status("shipped")
-		assert_equal 4843, results.count
+		assert_equal 1, results.count
 	end
 end
