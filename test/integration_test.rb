c45be25657f5bf76090a88eb63212f36e2a95ba4
Ian Andersen <ianderse@mac.com>
2014-08-27 13:31:31 -0600

    unskip tests

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -12,7 +12,6 @@ class IntegrationTest < Minitest::Test
 	end
 
 	def test_invoice_knows_if_it_is_pending
-		skip
 		invoice = @engine.invoice_repository.find_by_id(13)
 		pending_invoices = @engine.invoice_repository.pending
 
@@ -20,12 +19,10 @@ class IntegrationTest < Minitest::Test
 	end
 
 	def test_customer_knows_its_pending_invoices
-		skip
 		assert_equal [], @engine.customer_repository.find_by_id(2).pending_invoices
 	end
 
 	def test_customer_knows_days_since_last_activity
-		skip
 		date = Date.parse("March 29, 2012")
 		date2 = Date.parse("March 29, 2012")
 
@@ -34,24 +31,20 @@ class IntegrationTest < Minitest::Test
 	end
 
 	def test_customer_can_find_most_items
-		skip
 		assert_equal 622, @engine.customer_repository.most_items.id
 	end
 
 	def test_customer_can_find_most_revenue
-		skip
 		assert_equal 601, @engine.customer_repository.most_revenue.id
 	end
 
 	def test_it_can_find_revenue_for_specific_date
-		skip
 		merchant = @engine.merchant_repository.find_by_name "Willms and Sons"
 		date = Date.parse "Fri, 09 Mar 2012"
 		assert_equal BigDecimal.new("8373.29"), merchant.revenue(date)
 	end
 
 	def test_it_can_find_revenue_for_specific_date_merchant_repo
-		skip
 		date = Date.parse "Tue, 20 Mar 2012"
 		assert_equal BigDecimal.new("2549722.91"), @engine.merchant_repository.revenue(date)
 	end
@@ -62,7 +55,6 @@ class IntegrationTest < Minitest::Test
 	end
 
 	def test_it_can_find_pending_invoices_for_specific_merchants
-		skip
 		merchant = @engine.merchant_repository.find_by_name("Parisian Group")
 		customers = merchant.customers_with_pending_invoices
 
@@ -74,19 +66,16 @@ class IntegrationTest < Minitest::Test
 	end
 
 	def test_it_can_find_favorite_customer_for_specific_merchant
-		skip
 		merchant = @engine.merchant_repository.find_by_name("Terry-Moore")
 		assert_equal "Abernathy", merchant.favorite_customer.last_name
 	end
 
 	def test_it_can_find_item_with_most_revenue
-		skip
 		assert_equal "Item Dicta Autem", @engine.item_repository.most_revenue(5).first.name
 		assert_equal "Item Amet Accusamus", @engine.item_repository.most_revenue(5).last.name
 	end
 
 	def test_it_can_find_item_with_most_sold
-		skip
 		assert_equal "Item Nam Magnam", @engine.item_repository.most_items(37)[1].name
 	end
 
